1327fb71e788a46f88282fea79fa2873e3c7f514
takashi388412 <take388412@gmail.com>
2020-03-16 13:10:50 +0900

    add drawermenu

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
-// // モジュールとなるjsファイルのインポート
-// import { test_01 } from './js/test';
-// import { test_02 } from './js/test2';
+$(function () {
 
-// // モジュール先にある関数を実行
-// test_01();
-// test_02();
+  //hamberger_menu click時 動作 
+  // var sp_menu = $('.js-toggle-sp-menu');
+  // var sp_menu_target = $('.js-toggle-sp-menu-target');
+  var sp_menu = $('.hamberger-menu');
+  var sp_menu_target = $('.nav');
 
+  //hamberger-menu click動作
+  sp_menu.on('click', function () {
+    $(this).toggleClass('active');
+    sp_menu_target.toggleClass('active');
+  });
+  // // 【sp】リンク内クリック時の動作
+  // $('.nav-item a').on('click', function () {
+  //   sp_menu.removeClass('active');
+  //   sp_menu_target.removeClass('active');
+  // });
+});
